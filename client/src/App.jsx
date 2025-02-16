3f4cb53dc9ef7cf6ad4edaec4f3de561bbfa26f0
sameer <sameerbagul2004@gmail.com>
2025-02-16 15:45:06 +0530

    added some files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,39 @@ import EmailVerify from "./pages/EmailVerify";
 import ResetPassword from "./pages/ResetPassword";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import Sidebar from "./components/common/Sidebar";
+
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
+import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
+
+
 const App = () => {
   return (
     <div>
       <ToastContainer /> 
       <Routes>
+        {/* authentication Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/email-verify" element={<EmailVerify />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* Dashoard Route */}
+
+        <Route path="/OverviewPage" element={<OverviewPage />} />  
+        <Route path="/ProductsPage" element={<ProductsPage />} />
+        <Route path="/UsersPage" element={<UsersPage />} />
+        <Route path="/SalesPage" element={<SalesPage />} />
+        <Route path="/OrdersPage" element={<OrdersPage />} />
+        <Route path="/AnalyticsPage" element={<AnalyticsPage />} />
+        <Route path="/SettingsPage" element={<SettingsPage />} />
+        
       </Routes>
     </div>
   )
